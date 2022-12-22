57b5ef3b5dbd25f7fad8bc84e9e0fb492a6f96a7
Mikolaj Wancicki <miko12215@gmail.com>
2022-12-22 18:11:54 +0100

    test commit

---
+++ src/test/java/com/uuid/uuid/UuidApplicationTests2.java
@@ -0,0 +1,18 @@
+package com.uuid.uuid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class UuidApplicationTests2 {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void someTest(){
+	}
+
+}
