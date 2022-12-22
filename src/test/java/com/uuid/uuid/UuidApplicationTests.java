a0af1d7ca23d7064ead305a8c8ed2c078a4a5e87
Mikolaj Wancicki <miko12215@gmail.com>
2022-12-22 18:29:02 +0100

    test commit

--- src/test/java/com/uuid/uuid/UuidApplicationTests.java
+++ src/test/java/com/uuid/uuid/UuidApplicationTests.java
@@ -2,7 +2,9 @@ package com.uuid.uuid;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 class UuidApplicationTests {
 
